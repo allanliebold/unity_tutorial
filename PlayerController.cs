ea0f300d3d29c7d27c39ec1288d475796d137f3f
Allan Liebold <allan.liebold@gmail.com>
2018-01-06 15:47:40 -0800

    adds jumpHeight variable and condition for jump action on button press

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-  public float moveSpeed;
   private Rigidbody2D myRigidbody;
+  public float moveSpeed;
+  public float jumpHeight;
 
   void Start () {
     myRigidbody = GetComponent<Rigidbody2D>();
@@ -17,6 +18,10 @@ public class PlayerController : MonoBehaviour {
       myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
     } else {
       myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+    }
+
+    if(Input.GetButtonDown("Jump")){
+      myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpHeight, 0f);
     }
   }
 }
