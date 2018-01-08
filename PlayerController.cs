bc1f3bae469fe8a37c97281a1b1c678733dd9e19
Allan Liebold <allan.liebold@gmail.com>
2018-01-08 10:36:25 -0800

    added ground check variables and condition so player can't jump continuously in the air

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
   private Rigidbody2D myRigidbody;
+
   public float moveSpeed;
   public float jumpHeight;
+
+  public Transform groundCheck;
+  public float groundRadius;
+  public LayerMask groundLayer;
+  public bool isGrounded;
 
   void Start () {
     myRigidbody = GetComponent<Rigidbody2D>();
   }
 
   void Update () {
+    isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+
     if(Input.GetAxisRaw("Horizontal") > 0f) {
       myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
     }
@@ -20,7 +28,7 @@ public class PlayerController : MonoBehaviour {
       myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
     }
 
-    if(Input.GetButtonDown("Jump")){
+    if(Input.GetButtonDown("Jump") && isGrounded){
       myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpHeight, 0f);
     }
   }
