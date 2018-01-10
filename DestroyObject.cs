ac4e005855b9e2f778b06a068efdc9b33efb7f04
Allan Liebold <allan.liebold@gmail.com>
2018-01-10 13:46:29 -0800

    added destroy object script to clean up left over object instances when they are no longer in use

---
+++ DestroyObject.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour {
+
+  public float lifeTime;
+
+  void Update() {
+    lifeTime = lifeTime - Time.deltaTime;
+
+    if(lifeTime <= 0f) {
+      Destroy(gameObject);
+    }
+  }
+}
