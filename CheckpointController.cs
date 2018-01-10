141891b48ba2e7aed5b32907b8371eb241c85987
Allan Liebold <allan.liebold@gmail.com>
2018-01-10 01:04:30 -0800

    created CheckpointController. Sprite changes when player passes it

---
+++ CheckpointController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class CheckpointController : MonoBehaviour {
+
+  private SpriteRenderer spriteRenderer;
+  public Sprite flagClosed;
+  public Sprite flagOpen;
+
+  void Start() {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+
+  void OnTriggerEnter2D(Collider2D other) {
+    if(other.tag == "Player") {
+      spriteRenderer.sprite = flagOpen;
+    }
+  }
+}
