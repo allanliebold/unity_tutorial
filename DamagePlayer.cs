7066e0383ba0b1247794caf4417862f7a07f22d1
Allan Liebold <allan.liebold@gmail.com>
2018-01-11 05:01:21 -0800

    changed damage player script to call level manager's health down function, passing an amount of damage to deal

--- DamagePlayer.cs
+++ DamagePlayer.cs
@@ -4,14 +4,15 @@ using System.Collections;
 public class DamagePlayer : MonoBehaviour {
 
   private LevelManager levelManager;
-
+  public int damageAmount;
+  
   void Start() {
     levelManager = FindObjectOfType<LevelManager>();
   }
 
   void OnTriggerEnter2D(Collider2D other) {
     if(other.tag == "Player") {
-      levelManager.Respawn();
+      levelManager.HealthDown(damageAmount);
     }
   }
   
