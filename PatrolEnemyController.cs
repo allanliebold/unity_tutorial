131b2d39ad401d8f4186bc28cbec3862483200dd
Allan Liebold <allan.liebold@gmail.com>
2018-01-11 14:49:12 -0800

    created script for patrolling enemy that moves back and forth between two points

---
+++ PatrolEnemyController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class PatrolEnemyController : MonoBehaviour {
+
+  private Rigidbody2D enemyBody;
+
+  public Transform leftPoint, rightPoint;
+  public float moveSpeed;
+  public bool movingRight;
+
+  void Start() {
+    enemyBody = GetComponent<Rigidbody2D>();
+  }
+
+  void Update() {
+    if(movingRight && transform.position.x > rightPoint.position.x) {
+      movingRight = false;
+    }
+
+    if(!movingRight && transform.position.x < leftPoint.position.x) {
+      movingRight = true;
+    }
+
+    if(movingRight) {
+      enemyBody.velocity = new Vector3(moveSpeed, enemyBody.velocity.y, 0f);
+    } else {
+      enemyBody.velocity = new Vector3(-moveSpeed, enemyBody.velocity.y, 0f);
+    }
+  }
+
+}
