2e6e6a3a5f3c59e940e043b98b78934d2c64b756
Allan Liebold <allan.liebold@gmail.com>
2018-01-12 14:22:55 -0800

    created add life script for 1Up objects

---
+++ AddLife.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class AddLife : MonoBehaviour{
+
+  public int livesToAdd;
+  private LevelManager levelManager;
+
+  void Start() {
+    levelManager = FindObjectOfType<LevelManager>();
+  }
+
+  void OnTriggerEnter2D(Collider2D other) {
+    if(other.tag == "Player") {
+      levelManager.AddLives(livesToAdd);
+      gameObject.SetActive(false);
+    }
+  }
+}
