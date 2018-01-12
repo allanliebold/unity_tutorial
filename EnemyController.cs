0255e385d9fe381944d0ef5ce55c9fd3f217c66f
Allan Liebold <allan.liebold@gmail.com>
2018-01-11 20:51:05 -0800

    regular enemy is deactivated if it falls out of bounds

--- EnemyController.cs
+++ EnemyController.cs
@@ -5,26 +5,26 @@ public class EnemyController : MonoBehaviour {
 
   public float moveSpeed;
   private bool canMove;
-  
+
   private Rigidbody2D enemyBody;
-  
+
   void Start() {
     enemyBody = GetComponent<Rigidbody2D>();
   }
-  
+
   void Update() {
     if(canMove) {
       enemyBody.velocity = new Vector3(-moveSpeed, enemyBody.velocity.y, 0f);
     }
   }
-  
+
   void OnBecameVisible() {
     canMove = true;
   }
-  
+
   void OnTriggerEnter2D(Collider2D other) {
     if(other.tag == "Boundary") {
-      Destroy(gameObject);
+      gameObject.SetActive(false);
     }
   }
 }
