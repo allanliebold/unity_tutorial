bb05c0c4d1e905fae4c8904944b0c24204df941c
Allan Liebold <allan.liebold@gmail.com>
2018-01-11 18:17:47 -0800

    added bounce when player jumps on enemies

--- JumpAttack.cs
+++ JumpAttack.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class JumpAttack : MonoBehaviour {
 
+  private Rigidbody2D playerBody;
+  public float bounceAmount;
+
   public GameObject enemyDeath;
 
   void OnTriggerEnter2D(Collider2D other) {
     if(other.tag == "Enemy") {
       Destroy(other.gameObject);
       Instantiate(enemyDeath, other.transform.position, other.transform.rotation);
+
+      playerBody.velocity = new Vector3(playerBody.velocity.x, bounceAmount, 0f);
     }
   }
 }
