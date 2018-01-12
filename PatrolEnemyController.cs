d3d9c911e45779ee86fc680ce610b93f28af0ce4
Allan Liebold <allan.liebold@gmail.com>
2018-01-11 20:50:12 -0800

    patrolling enemy is deactivated if it falls out of bounds

--- PatrolEnemyController.cs
+++ PatrolEnemyController.cs
@@ -31,7 +31,7 @@ public class PatrolEnemyController : MonoBehaviour {
 
   void OnTriggerEnter2D(Collider2D other) {
     if(other.tag == "Boundary") {
-      Destroy(gameObject.transform.parent.gameObject);
+      gameObject.SetActive(false);
     }
   }
 
