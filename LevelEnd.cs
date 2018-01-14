6f2a39e9dfca767cadcdf982ab0efdb203ab96b8
Allan Liebold <allan.liebold@gmail.com>
2018-01-13 23:35:31 -0800

    changed collision enter 2D to trigger enter 2D in level end script

--- LevelEnd.cs
+++ LevelEnd.cs
@@ -6,7 +6,7 @@ public class LevelEnd : MonoBehaviour {
 
   public string levelName;
 
-  void OnCollisionEnter2D(Collider2D other) {
+  void OnTriggerEnter2D(Collider2D other) {
     if(other.tag == "Player") {
       SceneManager.LoadScene(levelName);
     }
