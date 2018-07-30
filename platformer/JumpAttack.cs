90d6e32c4ce316fb1d98cd4889a7f952588e1f5b
Allan Liebold <allan.liebold@gmail.com>
2018-07-30 06:14:38 -0700

    Update JumpAttack.cs

--- platformer/JumpAttack.cs
+++ platformer/JumpAttack.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 public class JumpAttack : MonoBehaviour {
-
   private Rigidbody2D playerBody;
   public float bounceAmount;
   public GameObject enemyDeath;
