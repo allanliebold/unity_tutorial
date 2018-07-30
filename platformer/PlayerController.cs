376f8be2ea3f42795341f9d3015db3c7d2423daa
Allan Liebold <allan.liebold@gmail.com>
2018-07-30 06:11:51 -0700

    Update PlayerController.cs

--- platformer/PlayerController.cs
+++ platformer/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour {
   
   void Update() {
     isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
+    isClimbing;
     
     if(Input.GetAxisRaw("Horizontal") > 0f) {
       playerBody.velocity = new Vector3(moveSpeed, playerBody.velocity.y, 0f);
