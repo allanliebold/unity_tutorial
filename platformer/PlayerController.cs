15fe5c4fb9fe6decb340e4624cc998a71af15e14
Allan Liebold <allan.liebold@gmail.com>
2019-06-08 20:58:54 -0700

    Update PlayerController.cs

--- platformer/PlayerController.cs
+++ platformer/PlayerController.cs
@@ -35,7 +35,7 @@ public class PlayerController : MonoBehaviour {
       playerBody.velocity = new Vector3(playerBody.velocity.x, jumpHeight, 0f);  
     }
     
-    playerBody.velocity = new Vector3();
+    playerBody.velocity = new Vector3(playerBody.velocity.x);
     
     animator.SetFloat("Speed", Mathf.Abs(playerBody.velocity.x));
     animator.SetBool("Grounded", isGrounded);
