f610a764fb3f7d66bd0d30a20de102119b41f369
Allan Liebold <allan.liebold@gmail.com>
2019-07-13 23:59:23 -0700

    Update PlayerController.cs

--- platformer/PlayerController.cs
+++ platformer/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour {
   }
   
   void Update() {
-    Grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
+    isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
     Climbing = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ladder);
     
     if(Input.GetAxisRaw("Horizontal") > 0f) {
