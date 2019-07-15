7a75773fb05695aa3e2dfdb308a81ec3c91f1f16
Allan Liebold <allan.liebold@gmail.com>
2019-07-14 21:37:27 -0700

    Update DamagePlayer.cs

--- platformer/DamagePlayer.cs
+++ platformer/DamagePlayer.cs
@@ -1,19 +1,1 @@
-using UnityEngine;
-using System.Collections;
 
-public class DamagePlayer : MonoBehaviour {
-
-  private LevelManager levelManager;
-  public int damageAmount;
-  
-  void Start() {
-    levelManager = FindObjectOfType<LevelManager>();
-  }
-
-  void OnTriggerEnter2D(Collider2D other) {
-    if(other.tag == "Player") {
-      levelManager.HealthDown(damageAmount);
-    }
-  }
-  
-}
